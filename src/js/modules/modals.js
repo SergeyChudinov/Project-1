96c552697eee290b391f5a33888df24734287a3d
Sergey Chudinov <sergeychudinov1986@gmail.com>
2022-09-22 18:54:39 +0500

    Make lesson-1-04

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -33,7 +33,7 @@ const modals = () => {
     }
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
-    showModalByTime('.popup', 3000)
+    // showModalByTime('.popup', 60000)
 
     // const modalTimerId = setTimeout(() => {
     //     document.querySelector('.popup').style.display = 'block';
